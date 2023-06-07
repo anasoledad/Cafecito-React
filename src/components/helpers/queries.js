2011cd06e3df883f36e7a482c94c10360567eaf5
Ana Rocha <anasoledad.rocha@gmail.com>
2023-06-07 04:18:25 -0300

    Se agregó la querie de login

---
+++ src/components/helpers/queries.js
@@ -0,0 +1,36 @@
+//llamar la variable de entorno
+const URL_USUARIO = import.meta.env.VITE_API_USUARIO;
+
+export const login = async (usuario)=>{
+    console.log(usuario)
+    try{
+        const respuesta = await fetch(URL_USUARIO);
+        const listaUsuarios = await respuesta.json();
+        console.log(listaUsuarios);
+        //buscar si en la listaUsuarios hay un usuario como el que recibi por parametro
+        const usuarioBuscado = listaUsuarios.find((itemUsuario)=>itemUsuario.email === usuario.email);
+        if(usuarioBuscado){
+            console.log('Email encontrado');
+            //verificar el password
+            if(usuarioBuscado.password === usuario.password){
+                console.log("encontramos al usuario!!!")
+                return usuarioBuscado;
+            }else{
+                console.log('Password incorrecto');
+                return null;
+            }
+        }else{
+            console.log('email incorrecto');
+            return null
+        }
+
+    }catch (error){
+        console.log(error);
+        return null
+    }
+}
+
+
+
+
+// const URL_PRODUCTO = import.meta.env.VITE_API_PRODUCTO
--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,22 +1,37 @@
 
 import { Form, Button, Container, Card } from "react-bootstrap";
+import { login } from "../helpers/queries";
+import { useForm } from "react-hook-form";
 
 
 const Login = () => {
+
+    const {register, handleSubmit, formState: {errors}, reset} = useForm();
+
+    
+
+    const onSubmit = (usuario)=>{
+        // console.log(usuario)
+        login(usuario);
+    }
+
   return (
     <Container className="mainSection">
       <Card className="my-5">
         <Card.Header as="h5">Login</Card.Header>
         <Card.Body>
-          <Form >
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type="email"
-                placeholder="Ingrese un email"
-               
+                placeholder="Ingrese un email" {
+                    ...register("email", {required: "El email es obligatorio"})
+                }        
               />
-             
+             <Form.Group className="text-danger">
+                {errors.email?.message}
+             </Form.Group>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -24,10 +39,14 @@ const Login = () => {
               <Form.Control
                 type="password"
                 placeholder="Password"
-               
+                {
+                    ...register("password", {required: "El email es obligatorio"})
+                }
               />
-            
             </Form.Group>
+            <Form.Group className="text-danger">
+                {errors.password?.message}
+             </Form.Group>
             <Button variant="primary" type="submit">
               Ingresar
             </Button>
