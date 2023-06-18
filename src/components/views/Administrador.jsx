6ec99bd3dd6cc38c96257a626fed7703605584e4
Ana Rocha <anasoledad.rocha@gmail.com>
2023-06-18 04:34:36 -0300

    Se agregó la funcionalidad para borrar y recargamos la tabla de productos

--- src/components/views/Administrador.jsx
+++ src/components/views/Administrador.jsx
@@ -36,7 +36,7 @@ const Administrador = () => {
           </thead>
           <tbody>
             {
-              productos.map((producto)=><ItemProducto key={producto.id} producto={producto}></ItemProducto>)
+              productos.map((producto)=><ItemProducto key={producto.id} producto={producto} setProductos={setProductos}></ItemProducto>)
             }
           </tbody>
         </Table>
